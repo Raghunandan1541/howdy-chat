260c8095330c4e2d9e550acffe2fb60c568aa0df
Raghunandan <raghunandan1541.cse18@chitkara.edu.in>
2021-06-14 08:39:29 +0530

    Created another branch

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -4,13 +4,14 @@ import './Login.css';
 function Login() {
 	return (
 		<div className="login">
-			<div className="login__header">Enter Name</div>
+			{/* <div className="login__header">Enter Name</div>
 			<div className="login__input">
 				<form>
 					<input placeholder="Enter your name" />
 					<button value="Sign In" />
 				</form>
-			</div>
+			</div> */}
+			<h1>LOGIN</h1>
 		</div>
 	);
 }
